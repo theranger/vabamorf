bf411bd2a47a78440bee6055db66a7d803b6a73d
The Ranger <ranger@risk.ee>
2016-12-26 20:34:38 +0200

    Refactor code to be used as a library.

--- java/src/ee/risk/vabamorf/JVabamorf.java
+++ java/src/ee/risk/vabamorf/JVabamorf.java
@@ -8,24 +8,39 @@ import ee.risk.vabamorf.model.Word;
  */
 public class JVabamorf {
 
-	public native void analyze(String linguisticDict, String disambiguatorDict, Sentence sentence);
+	public native void analyze(String linguisticDict, String disambiguatorDict, Sentence sentence) throws LinguisticException, DisambiguatorException;
 	public native String getModuleName();
+
+	private String linguisticDict;
+	private String disambiguatorDict;
 
 	static {
 		System.loadLibrary("vabamorf-jni");
 	}
 
+	public JVabamorf(String linguisticDict, String disambiguatorDict) {
+		this.linguisticDict = linguisticDict;
+		this.disambiguatorDict = disambiguatorDict;
+	}
+
+	public Sentence parseSentence(String text) throws LinguisticException, DisambiguatorException {
+		Sentence sentence = new Sentence(text);
+		this.analyze(linguisticDict, disambiguatorDict, sentence);
+		return sentence;
+	}
+
 	public static void main(String[] args) throws LinguisticException, DisambiguatorException {
 		if (args.length !=3) {
-			System.err.println("Usage: JVabamorf <et.dct> <et3.dct> <corpus_string>");
+			System.err.println("Usage: JVabamorf <et.dct> <et3.dct> <sentence>");
 			return;
 		}
 
-		JVabamorf jVabamorf = new JVabamorf();
-		Sentence sentence = new Sentence("Vihmase ilmaga me õue ei lähe");
+		JVabamorf jVabamorf = new JVabamorf(args[0], args[1]);
 
 		System.out.println(jVabamorf.getModuleName());
-		jVabamorf.analyze(args[0], args[1], sentence);
+		System.out.println("Analyzing [" + args[2] + "]");
+
+		Sentence sentence = jVabamorf.parseSentence(args[2]);
 
 		for (Word w : sentence.getWords()) {
 			System.out.println("[" + w.getData() + "]");
